aeb5fecde8756d11ac6094ce2c5d9481bb2e1c6c
pixelzoom <cmalley@pixelzoom.com>
2024-03-13 16:39:37 -0600

    doc

--- js/magnet-and-compass-main.ts
+++ js/magnet-and-compass-main.ts
@@ -1,7 +1,7 @@
 // Copyright 2024, University of Colorado Boulder
 
 /**
- * Main entry point for the sim.
+ * Main entry point for the 'Magnet and Compass' sim.
  *
  * @author Chris Malley (PixelZoom, Inc.)
  */
